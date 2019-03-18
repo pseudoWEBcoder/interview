1c5aab7fa2e8939e4fcdbe992962d809e3ad33bc
alal <betu@maiola.net>
2019-03-19 00:33:59 +0300

    добавил  возможность создавать  вопросы  с чекбоксами но   ответ оно не  понимает как  считать пока что

--- js/main.js
+++ js/main.js
@@ -8,23 +8,23 @@ jQuery(function () {
             'q': 'семь красных линий зеленым цветом нарисуешь?а перпендикулярных',
             'a': 'да конечно',
             'v': ['нет', 'это не возможно', 'не уверен', 'не знаю'], /*варианты*/
-            't': 0
+            't': 1
         },
         ], /*список  вопросов*/
         next = $('#next')/*кнопка далее*/,
-        Questions = $('#Questions')
-    /*область вопросов*/
-    current = 0, /*указатель на  текущий вопрос*/
-
+        Questions = $('#Questions') /*область вопросов*/,
+        current = 0/*указатель на  текущий вопрос*/,
         create = function (Question)/*функция создания вопроса*/ {
             type = 't' in Question ? Question.t : 0;//получаем  тип, если не задан значит один вариант  ответа
-
             wrapper = $('<form class="wrapper form-"  data-current="' + current + '"/>');
             p = $('<p/>');
             list = $('<ul />'); //html элемент со списком ответов
             ;
             p.html('<strong>' + Question.q + '</strong>');
-            if (type == 0) {
+            if (type == 0 || type == 1) {
+                types = ['radio', 'checkbox'];
+                type = types[type]
+
                 $.each(Question.v, function (i, v) {
                     let li = $('<li class="form-group"/>'), label = $('<label>'), inp = $('<input>'),
                         id = 'Question_' + current + '_answer_' + i;
@@ -32,22 +32,23 @@ jQuery(function () {
                     label.attr({'for': id}).text(v)
                     inp.attr({
                         'class': 'form-control',
-                        'type': 'radio',
+                        'type': type,
                         'id': id,
-                        'name': 'Question[' + current + ']',
-                        'value': i
+                        'name': (type == 'radio' ? 'Question[' + current + ']' : 'Question[' + current + '][' + i + ']'),
+                        'value': (type == 'radio' ? i : '')
                     });
                     li.append(label)
                     li.append(inp)
                     list.append(li)
                 })
-            } else {
+            } else if (type == 2) {
+
+            }
+            else {
             }
             wrapper.append(p)
             wrapper.append(list)
             Questions.html(wrapper[0].outerHTML);
-
-
         },
         renderResult = function ()/*рисуем таблицу  результатов*/ {
             table = $('<table class="table table-bordered">');
@@ -64,17 +65,25 @@ jQuery(function () {
                 key = i - 1;
                 if (typeof Dict[key] == 'undefined')
                     return true;//нет  такого вопроса
+                type = 't' in Dict[key] ? Dict[key].t : 0;
+                if (type == 0) {
+                    A = Dict[key].v[answer]
+                } else if (type == 1) {
+                    let Items = []
+                    $.each(v.result, (ii, vv) => {/*сттрелочная функция, фишка 2018*/
+                        xach = vv.name.match(/(.+)]\[(\d+)\]/)//регулярное выражение вырезает  последний номер в квадратных скобках из  имени
+                        Items.push('<li>' + Dict[key].v[xach[2]] + '</li>')
+                    })
+                    A = '<ul>' + Items.join('\n') + '</ul>';
+                }
                 tr = $('<tr/>')
                 tr = tr.append(($('<td/>').text(Dict[key].q)))// вопрос
                 tr = tr.append(($('<td/>').text(Dict[key].a)))//правильный овет
-                tr = tr.append(($('<td/>').text(Dict[key].v[answer])))//ваш ответ
+                tr = tr.append(($('<td/>').html(A)))//ваш ответ
                 table.append(tr)
-
-
             });
             Questions.html(table[0].outerHTML);
         }, Result = [],     //массив  результатов
-
         goNext = function ()/*функция обработки перехода на следующий шаг*/ {
             let r = {'result': $('.wrapper').serializeArray(), 'start': new Date};
 
